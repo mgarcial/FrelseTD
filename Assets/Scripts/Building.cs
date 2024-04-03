17fa0229eac685b0ea037eddab4a28c27d747272
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2024-04-03 14:55:56 -0500

    Building&WeaponSettings

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public int cost;
+    private Weapon _weapon;
+    private bool _isHover = false;
     public int rate;
+    
 
     GameManager gm = GameManager.instance;
 
     private void Awake()
     {
         gm.SumGoldRate = rate;
+        _weapon = GetComponent<Weapon>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void DestroyTower()
+    {
+        
     }
 }
