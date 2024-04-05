9ab32d0730379c0f88ecc5814573415888b6d818
Garcial <miguelgarcialeiva@gmail.com>
2024-04-05 12:59:15 -0500

    Integrando un poquito
    Probando los prefabs que hizo jose y cambiando los terminos de oro por circuitos.
    
    Moví la mayoría de las cosas a la escena "NavMesh" pero los botones no estan funcionando, prefab o no

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -9,18 +9,29 @@ public class Enemigo : MonoBehaviour
     [SerializeField] private Transform nextPoint;
 
     private NavMeshAgent navMeshAgent;
+    private bool empezar;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.updateUpAxis = false;
         navMeshAgent.updateRotation = false;
+        empezar = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(nextPoint.position);
+        if (empezar)
+        {
+            navMeshAgent.SetDestination(nextPoint.position);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            empezar = true;
+        }
     }
 
     internal void TakeDamage(object p)
