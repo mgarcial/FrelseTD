387ad1f7538e2bba8a2ad631822d31f505740d42
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2024-05-01 20:05:18 -0500

    TrailRenderer&CorrectedRotation

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,11 +10,12 @@ public class Projectile : MonoBehaviour
     private Rigidbody2D _rb;
     private DamageDeal _damageDeal;
     private Transform _target;
-
+    private TrailRenderer _trailRenderer;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _damageDeal = GetComponent<DamageDeal>();
+        _trailRenderer = GetComponent<TrailRenderer>();
     }
 
     private void FixedUpdate()
@@ -30,11 +31,21 @@ public class Projectile : MonoBehaviour
     {
         if (_target != null)
         {
-            transform.LookAt(_target.position);
-            _rb.velocity = transform.forward * speed;
-            Vector2 dir = _target.position - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(-25, 0, angle);
+            Vector2 direction = (_target.position - transform.position).normalized;
+
+            // Set velocity based on the calculated direction and speed
+            _rb.velocity = direction * speed;
+
+            // Calculate rotation angle to point towards the target
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            angle += 270f;
+
+            // Apply rotation to the arrow's transform (only rotate around Z-axis)
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            if (!_trailRenderer.enabled)
+                _trailRenderer.enabled = true;
         }
         else
         {
